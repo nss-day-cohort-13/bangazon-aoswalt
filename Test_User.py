f3b048dead6a46077c41082766e5cae7f5c38add
Adam Oswalt <adam.oswalt@gmail.com>
2016-08-01 15:12:38 -0500

    test user str and repr

--- Test_User.py
+++ Test_User.py
@@ -7,7 +7,7 @@ class TestUser(unittest.TestCase):
 
     def test_user_id_increment(self):
         User.next_user_id = 1
-        
+
         self.assertEqual(User.next_user_id, 1)
 
         User('Test Person', 'tp')
@@ -15,6 +15,20 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(User.next_user_id, 3)
 
+    def test_user_str(self):
+        User.next_user_id = 1
+
+        user = User('That Guy', 'guy')
+
+        self.assertEqual(str(user), 'guy - That Guy')
+
+
+    def test_user_repr(self):
+        User.next_user_id = 1
+
+        user = User('That Guy', 'guy')
+
+        self.assertEqual(repr(user), '1. guy - That Guy')
 
 if __name__ == '__main__':
     unittest.main()
